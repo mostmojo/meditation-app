b142174feb10cbc546490d3e0722f18b94078df9
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-22 16:21:18 +0000

    Set timer to side buttons to correspond to timer in sound column:

--- app.js
+++ app.js
@@ -8,22 +8,59 @@ const app = () => {
   const sounds = document.querySelectorAll('.sound-picker button');
   // Time display, h3 tag
   const timeDisplay = document.querySelector('.time-display');
+  //Time select all the buttons with their duration
+  const timeSelect = document.querySelectorAll('.time-select button');
   // Get the length of the outline
   const outlineLength = outline.getTotalLength();
   // Duration
   let fakeDuration = 600;
-
+  // Stripes around the play/pause button (later will become progress bar)
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
   // Play sound
   play.addEventListener('click', () => {
-      song.play();
-    
+      checkPlaying(song);
+    });
+
+    // Select sound
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function () {
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+                fakeDuration % 60
+                )}`;
+        });
     });
 
     // Create a function specific to stop and play the sounds
-    
+    const checkPlaying = song => {
+        if(song.paused){
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg'; 
+        }else{
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    }
+    // We can animate the circle around the play button to show song 'progress'
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60); 
+
+        // Animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        // Animate the text 
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+    };
 };
 
 app();
+
+
+//Math.floor rounds up the numbers to whole numbers 
